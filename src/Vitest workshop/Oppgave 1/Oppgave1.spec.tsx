4cf48e3db14c234c6b8648f8ce2030a93d9706c8
Ian Evangelista <ianevangelista1999@gmail.com>
2025-02-25 15:16:22 +0100

    add link

--- src/Vitest workshop/Oppgave 1/Oppgave1.spec.tsx
+++ src/Vitest workshop/Oppgave 1/Oppgave1.spec.tsx
@@ -1,5 +1,6 @@
 // Oppgave 1:
-// Bruk sum-funksjonen under og lag en test der du forventer at summen blir riktig.
+// Bruk sum-funksjonen under og lag en test der du forventer at summen blir riktig ved bruk av vitest.
+// https://vitest.dev/guide/
 
 function sum(a: number, b: number) {
   return a + b;
