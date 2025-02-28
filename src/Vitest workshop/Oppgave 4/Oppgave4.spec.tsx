c75be7b1e1905f334774acfbdad55fe8e21a06e4
Ian Evangelista <ianevangelista1999@gmail.com>
2025-02-28 16:44:24 +0100

    update oppgave 4

--- src/Vitest workshop/Oppgave 4/Oppgave4.spec.tsx
+++ src/Vitest workshop/Oppgave 4/Oppgave4.spec.tsx
@@ -1,18 +1,19 @@
 // Oppgave 4:
 // Lag en test som sjekker hvor mange ganger console.log blir kalt på når logAllUsers kjøres
+// Tips: her må du nok sjekke ut mock functions i Vitest
 
 type User = {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 };
-  
+
 const users: User[] = [
-    { id: 1, name: 'Juni' },
-    { id: 2, name: 'Ian' }
+  { id: 1, name: "Juni" },
+  { id: 2, name: "Ian" },
 ];
 
 export function logAllUsers(): void {
-    for (const user of users) {
-        console.log(user.name)
-    }
-}  
+  for (const user of users) {
+    console.log(user.name);
+  }
+}
