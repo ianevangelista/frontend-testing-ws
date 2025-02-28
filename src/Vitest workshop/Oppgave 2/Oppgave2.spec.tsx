576065d1c06519f40c95e1fc0d6be50a2596efaa
Ian Evangelista <ianevangelista1999@gmail.com>
2025-02-28 16:31:26 +0100

    update oppgave 2

--- src/Vitest workshop/Oppgave 2/Oppgave2.spec.tsx
+++ src/Vitest workshop/Oppgave 2/Oppgave2.spec.tsx
@@ -1,9 +1,10 @@
-// Oppgave 2:
+// Oppgave 2.1:
 // Test getUserInfo returnerer ønsket objekt
 
 export function getUserInfo(): { name: string; age: number } {
-    return { name: 'Ian', age: 45 };
-  }
+  return { name: "Ian", age: 45 };
+}
 
-// Oppgave 2.1 - Til refleksjon:
+// Oppgave 2.2 - Til refleksjon:
+// Hva skjer hvis du bruker toEqual()?
 // Hva er forskjellen på bruk av toBe() og toEqual() i Vitest?
--- src/Vitest workshop/Oppgave 2/Oppgave2Fasit.spec.tsx
+++ src/Vitest workshop/Oppgave 2/Oppgave2Fasit.spec.tsx
@@ -1,10 +1,7 @@
-// Oppgave 2
-import { test, expect } from 'vitest';
-import { getUserInfo } from './Oppgave2.spec';
+import { expect, test } from "vitest";
+import { getUserInfo } from "./Oppgave2.spec";
 
-test('getUserInfo should return the correct object', () => {
+test("getUserInfo should return the correct object", () => {
   const userInfo = getUserInfo();
-  expect(userInfo).toEqual({ name: 'Ian', age: 45 });
-
-  expect(userInfo).not.toBe({ name: 'Ian', age: 45 }); // Legg merke til "not.toBe"
+  expect(userInfo).toEqual({ name: "Ian", age: 45 });
 });
