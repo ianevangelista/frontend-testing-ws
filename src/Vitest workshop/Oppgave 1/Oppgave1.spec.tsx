68506d26060e5077499f703b27e3724aa1b8be62
Ian Evangelista <ianevangelista1999@gmail.com>
2025-02-28 17:14:13 +0100

    update oppgave 1

--- src/Vitest workshop/Oppgave 1/Oppgave1.spec.tsx
+++ src/Vitest workshop/Oppgave 1/Oppgave1.spec.tsx
@@ -1,7 +1,7 @@
 // Oppgave 1:
-// Bruk sum-funksjonen under og lag en test der du forventer at summen blir riktig ved bruk av vitest.
+// Bruk sum-funksjonen og lag en test der du forventer at summen blir riktig ved bruk av vitest.
 // https://vitest.dev/guide/
 
-function sum(a: number, b: number) {
+export function sum(a: number, b: number) {
   return a + b;
 }
--- src/Vitest workshop/Oppgave 1/Oppgave1Fasit.spec.tsx
+++ src/Vitest workshop/Oppgave 1/Oppgave1Fasit.spec.tsx
@@ -1,8 +1,5 @@
 import { expect, test } from "vitest";
-
-function sum(a: number, b: number) {
-  return a + b;
-}
+import { sum } from "./Oppgave1.spec";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
